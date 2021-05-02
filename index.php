26b2e1a6f3321755cf8834399c20144a6e141477
Günce Bektaş <gunce@binalyze.com>
2021-05-02 20:48:18 +0300

    minor upadtes

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="shortcut icon" href="favicon.png" type="image/png">
+  <meta name="google-site-verification" content="01JQJkrJf7n6dZfEhm9O4xthi1UeFNHrKMV0O1YtRT0"/>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+  
+  <title>Davetiye | Nikah 25 Haziran / Düğün 01 Temmuz</title>
+  <meta name="description" content="Sizleri bu mutlu günümüzde aramızda görmekten mutlu oluruz.">
+  <meta name="author" content="Gunce Bektas">
+  <meta name="email" content="guncebektas@gmail.com">
+  <meta name="copyright" content="Gunce Bektas 2021"/>
+  
+  <meta property="og:type" content="website"/>
+  <meta property="og:title" content="Davetiye | Nikah 25 Haziran / Düğün 01 Temmuz"/>
+  <meta property="og:description" content="Sizleri bu mutlu günümüzde aramızda görmekten mutlu oluruz."/>
+  <meta property="og:image" content="https://user-images.githubusercontent.com/53209422/109410448-d1794700-79c0-11eb-93fa-5bf351190eaa.png"/>
+  <meta property="og:url" content=""/>
+  <meta property="og:site_name" content="dugun.guncebektas.com"/>
+  
+  <meta name="twitter:card" content="website">
+  <meta name="twitter:site" content="dugun.guncebektas.com">
+  <meta name="twitter:title" content="Davetiye | Nikah 25 Haziran / Düğün 01 Temmuz">
+  <meta name="twitter:description" content="Sizleri bu mutlu günümüzde aramızda görmekten mutlu oluruz.">
+
+  <link href="https://fonts.googleapis.com/css?family=Arvo&subset=latin,latin-ext" rel="stylesheet">
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Dancing+Script&subset=latin,latin-ext'>
+  <link rel="stylesheet" href="./css/style.css?<?php echo rand(0, 2000); ?>">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="sakura-falling"></div>
+
+    <img src="assets/img/right.png" alt="image-top-right" class="top-right-decoration">
+    <img src="assets/img/left.png" alt="image-top-left" class="top-left-decoration">
+
+    <section id="media"></section>
+
+    <div class="wrap">
+      <div class="title">
+        <h1 style="margin-bottom: 10px">Neşe </h1>
+        <h2>&</h2>
+        <h1 style="margin-top: 0">Günce</h1>
+        <h3>Biz Evleniyoruz</h3>
+        <h3>
+          <p>25 Haziran 2021, Cuma</p>
+          <p>Bedesten Nikah Salonu Şehzadeler / Manisa </p>
+          <p>Saat 15.00</p>
+        </h3>
+      </div>
+    </div>
+
+    <div class="actions">
+      <a href="https://docs.google.com/forms/d/1FgaPscYLdiRsZsD9JJqy4RYgvnp5urKf8UpxNO0ECzk" target="_blank">
+        <div class="venue">LCV</div>
+      </a>
+
+      <a href="https://www.google.com/maps/place/Bedesten+Nikah+Salonu/@38.6114554,27.4320537,19.34z/data=!4m5!3m4!1s0x0:0x4b5ba683a948e647!8m2!3d38.6114073!4d27.4323292" target="_blank">
+        <div class="venue">Harita</div>
+      </a>
+
+      <a href="https://github.com/arungop/akhiarya/files/6328859/akhiwedsarya.pdf" download="Davetiye">
+        <div class="venue">Davetiye</div>
+      </a>
+    </div>
+
+    <div class="music">
+      <audio src="./assets/mp3/music.mp3" id="my_audio" loop="loop"></audio>
+    </div>
+
+    <footer>
+      <div class="containerw3 containerw3-margin">
+        <a href="javascript:void(0);">
+          <img src="assets/img/p6.jpg"/>
+        </a>
+        <a href="javascript:void(0);">
+          <img src="assets/img/p7.jpg"/>
+        </a>
+        <a href="javascript:void(0);">
+          <img src="assets/img/p4.jpg"/>
+        </a>
+        <a href="javascript:void(0);">
+          <img src="assets/img/p3.jpg"/>
+        </a>
+        <a href="javascript:void(0);">
+          <img src="assets/img/p2.jpg"/>
+        </a>
+        <a href="javascript:void(0);">
+          <img src="assets/img/p1.jpg"/>
+        </a>
+      </div>
+    </footer>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+    <script src='https://cdn.jsdelivr.net/gh/timoschaefer/jQuery-Sakura/jquery-sakura.min.js'></script>
+    <script src="./js/script.js"></script>
+</body>
+</html>
